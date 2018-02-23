bec4038347fdc3e6f8b7ac42bafdfd11299b79d7
Student <student@ib.polsl.pl>
2018-02-23 10:48:38 +0100

    Trzeci commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
+#include<LaborkaConfig.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include <trygonometria.h>
-int main() { /*...*/
-	
+#else
+#define _USE_MATH_DEFINES
+#include <cmath>
+#endif
+
+
+
+int main() { 
+
+	#ifdef USE_TRIGONOMETRY_DEGREE
 	double wyniksin=degreemath::sin(50.0);
 	double wynikcos=degreemath::cos(50.0);
 	double wyniktg=degreemath::tg(50.0);
 	double wynikctg=degreemath::ctg(50.0);
+
+#else
+	double wyniksin=sin(M_PI/4.0);
+	double wynikcos=cos(M_PI/4.0);
+	double wyniktg=tan(M_PI/4.0);
+	double wynikctg=1/wyniktg;
+
+#endif
 	
-	
+
 	std::cout<<"sin:"<<std::endl;
 	std::cout<<wyniksin<<std::endl;
 	std::cout<<"cos:"<<std::endl;
